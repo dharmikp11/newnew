067d992e49ef3dd8c3560cfbba0a480d419e7a0e
Dharmik <dharmikp11@gmail.com>
2024-02-02 13:26:25 +0530

    Initial commit

---
+++ index-1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIT TEST</title>
+</head>
+<body>
+    <h1>Heading</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ea dolorem perspiciatis aliquid autem placeat, laboriosam minus animi beatae dolor harum quo excepturi eaque ut. Nam debitis expedita iure ea eveniet facere, necessitatibus animi qui obcaecati voluptatem perspiciatis tempore est, incidunt odio labore quo officia laboriosam magnam earum itaque consectetur accusantium officiis veniam natus. Consequuntur iusto id quae ipsum nihil repellendus, eum aspernatur? Fuga omnis quae deleniti culpa, nostrum dolor cumque minus, commodi quasi aperiam molestias sapiente veniam nemo, qui repudiandae! Nam iure, error nesciunt ipsam cumque aspernatur consequatur aliquid distinctio voluptatem, numquam facilis quidem officiis libero nulla. Debitis, maxime?</p>
+</body>
+</html>
