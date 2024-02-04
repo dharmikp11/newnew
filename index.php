abf0a240472cc927a92c8610e0841f02a25927b8
Dharmik <dharmikp11@gmail.com>
2024-02-04 12:18:23 +0530

    This is commit to test github push

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <title>Hello</title>
 </head>
 <body>
-    <h1>Hello! 2</h1>
-    <p>Lorem ipsgdfgvcvum dolor sit, amet consectetur adipisicing elit. Quibusdam, odio.</p>
+    <h1>Hello!</h1>
+    <p>This is the test code to push files to online branch.</p>
     <h6>Hello, this is h6 tag</h6>
 </body>
 </html>
